d201e4a63e0f9b47b1df44b6104713512509647a
Ibnlaouale <dinilaouale@gmail.com>
2024-05-01 01:25:58 +0200

    check the valable matrix

--- matrix/validMatrix.js
+++ matrix/validMatrix.js
@@ -1,26 +1,32 @@
 const validmatrix = (m) => {
-    let sum = 0;
-  let invalidTable = m.find((table) => table.length !== m[0].length);
+  let invalidTable,
+    notNumber = [];
 
-  for (let i = 0; i < m.length; i++) {
-    for (let j = 0; j < m[i].length; j++) {
-        sum += m[i][j]
+  invalidTable = m.find((table) => table.length !== m[0].length);
+
+  for (let k = 0; k < m.length; k++) {
+    for (let l = 0; l < m[k].length; l++) {
+      if (typeof m[k][l] !== "number") {
+        notNumber.push(m[k][l]);
+      }
     }
   }
 
-  if (invalidTable) {
+  // console.log("type ====>", checkType,invalidTable, notNumber, notNumber.length);
+  if ((invalidTable || !invalidTable) && notNumber.length > 0) {
     console.log("matrix invalid");
-   
+    return;
   } else {
     console.log("matrix valid");
+    return
   }
 };
 
 let m = [
-  [1, 4, , 3],
-  [0, 9, 8, 7],
-  [12, 33, 44, 22],
-  [2, 2, 5, 2],
+  [1, 4, 2, 3],
+  [0, 9, 3, 7],
+  [12, , 44, 22],
+  [2, 3, 5, 2],
 ];
 
 validmatrix(m);
